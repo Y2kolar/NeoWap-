61e5c72f2b940fe101fc8049b9d19d58bc053764
Y2kolar <108243286+Y2kolar@users.noreply.github.com>
2026-05-02 15:26:29 +0300

    Implement CORS middleware in server.js
    Added CORS headers to server responses.

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ const { Server } = require("socket.io");
 
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
